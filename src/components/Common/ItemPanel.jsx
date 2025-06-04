7e7da107d1530a4762fa98cf6e53874784499fcf
heeya314 <heeya314@naver.com>
2025-06-04 10:48:54 +0900

    first commit

---
+++ src/components/Common/ItemPanel.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import Item from './Item';
+import { useDispatch, useSelector } from 'react-redux';
+import AddItem from './AddItem';
+import PageTitle from './PageTitle';
+import { fetchGetItems } from '../../redux/slices/apiSlice';
+import { SkeletonTheme } from 'react-loading-skeleton';
+import LoadingSkeleton from './LoadingSkeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+const ItemPanel = ({ pageTitle }) => {
+  // Auth Data Variables
+  const authData = useSelector((state) => state.auth.authData);
+  const userKey = authData?.sub;
+  // console.log(userKey); 100775200008845602401
+
+  // API Data Variable
+  const getTasksData = useSelector((state) => state.api.getItemsData);
+  const dispatch = useDispatch();
+
+  // loading state
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!userKey) return;
+
+    const fetchGetItemsData = async () => {
+      try {
+        setLoading(true);
+        // useEffect 내부에서 dispatch 함수를 호출할 때는 async/await를 사용할 수 없을때 unwrap()을 사용;
+        await dispatch(fetchGetItems(userKey)).unwrap();
+      } catch (error) {
+        console.log('Fail to fetch items', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchGetItemsData();
+  }, [dispatch, userKey]);
+
+  // console.log(getTasksData);
+  return (
+    <div className="panel bg-[#212121] w-4/5 h-full rounded-md border border-gray-500 py-5 px-4 overflow-y-auto">
+      {userKey ? (
+        <div className="login-message w-full h-full">
+          <PageTitle title={pageTitle} />
+          <div className="flex flex-wrap">
+            {/* {getTasksData?.map((task, idx) => (
+              <Item key={idx} task={task} />
+            ))} */}
+
+            {loading ? (
+              <SkeletonTheme
+                baseColor="#202020"
+                highlightColor="#444"
+                height="25vh"
+              >
+                {/* {[...Array(getTasksData?.length)].map((_, idx) => (
+                  <LoadingSkeleton key={idx} />
+                ))} */}
+                <LoadingSkeleton />
+                <LoadingSkeleton />
+                <LoadingSkeleton />
+              </SkeletonTheme>
+            ) : (
+              getTasksData?.map((task, idx) => <Item key={idx} task={task} />)
+            )}
+
+            <AddItem />
+          </div>
+        </div>
+      ) : (
+        <div className="login-message w-full h-full flex items-center justify-center">
+          <button className="flex justify-center items-center gap-2 bg-gray-300 text-gray-900 py-2 px-4 rounded-md">
+            <span className="text-sm font-semibold">
+              로그인이 필요한 서비스 입니다.
+            </span>
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ItemPanel;
---
+++ src/utils/apiUrl.js
@@ -0,0 +1,6 @@
+const DOMAIN = 'https://backend.heeya.store';
+
+const GET_TASKS_API_URL = `${DOMAIN}/get_task`;
+const UPDATE_COMPLETED_TASK_API_URL = `${DOMAIN}/update_completed_task`;
+
+export { GET_TASKS_API_URL, UPDATE_COMPLETED_TASK_API_URL };
